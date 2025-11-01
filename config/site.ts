b6c81c427e8cb3db798d6c31f3e0c5cbebe7d36c
belpythons <zerobocilicung393@gmail.com>
2025-11-01 14:50:34 +0800

    feat(config): Update site metadata and main page content

--- config/site.ts
+++ config/site.ts
@@ -1,42 +1,27 @@
 export const siteConfig = {
-  name: "Naman Barkiya | Developer Portfolio",
-  authorName: "Naman Barkiya",
-  username: "namanbarkiya",
+  name: "Belva Pranama Sriwibowo | Developer Portfolio",
+  authorName: "Belva Pranama Sriwibowo",
+  username: "belpythons",
   description:
-    "Naman Barkiya's portfolio - A modern, responsive Next.js portfolio for developers. This open-source template can be customized to showcase your own skills, projects, and professional experience.",
-  url: "https://nbarkiya.xyz",
+    "Halo! Saya Belva Pranama Sriwibowo, seorang mahasiswa Teknik Informatika di Sekolah Tinggi Teknologi Bontang (STITEK Bontang). Saya adalah seorang pembelajar yang adaptif dan selalu antusias untuk mengeksplorasi hal-hal baru, terutama dalam lanskap teknologi yang terus berkembang. Minat utama saya terletak pada dunia Teknologi Informasi dan Sistem Informasi, di mana saya bersemangat untuk berkontribusi dan memecahkan masalah.",
+  url: "https://github.com/belpythons", // Ganti dengan URL domain Anda jika punya
   links: {
-    twitter: "https://x.com/namanbarkiya",
-    github: "https://github.com/namanbarkiya",
+    twitter: "https://x.com/bekgos8955?t=UVTHOJDdUwvAvso2N_Rrsg&s=08",
+    github: "https://github.com/belpythons",
   },
-  ogImage: "https://nbarkiya.xyz/profile-img.jpg",
-  iconIco:
-    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
-  logoIcon:
-    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
+  ogImage: "/profile-img.jpg", // Pastikan Anda mengganti file ini di /public
+  iconIco: "/favicon.ico", // Pastikan Anda mengganti file ini di /public
+  logoIcon: "/logo.png", // Pastikan Anda mengganti file ini di /public
   keywords: [
-    "Naman Barkiya",
+    "Belva Pranama Sriwibowo",
     "Full Stack Developer",
-    "Web Developer Portfolio",
-    "Next.js portfolio template",
-    "React portfolio template",
-    "Developer portfolio template",
-    "Portfolio website template",
-    "Next.js template",
-    "Modern portfolio website",
-    "Frontend developer portfolio",
-    "Full stack developer portfolio",
-    "Open source portfolio template",
-    "Web developer portfolio template",
-    "Responsive portfolio template",
-    "React.js portfolio",
-    "Tailwind CSS portfolio",
-    "Developer showcase template",
-    "Free portfolio template",
-    "Next.js 13 template",
-    "Shadcn UI template",
-    "Career timeline template",
-    "Portfolio with dark mode",
-    "GitHub portfolio template",
+    "Desainer UI/UX",
+    "Portofolio Developer",
+    "MERN Stack",
+    "Laravel",
+    "React",
+    "PHP",
+    "STITEK Bontang",
+    "Mahasiswa Teknik Informatika",
   ],
 };
