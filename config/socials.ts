19ccc7fbb70245c67f5150ed8a0e528c2db5516f
belpythons <zerobocilicung393@gmail.com>
2025-11-01 15:03:57 +0800

    feat(data): Populate portfolio with personal data

--- config/socials.ts
+++ config/socials.ts
@@ -10,26 +10,26 @@ interface SocialInterface {
 export const SocialLinks: SocialInterface[] = [
   {
     name: "Github",
-    username: "@namanbarkiya",
+    username: "@belpythons",
     icon: Icons.gitHub,
-    link: "https://github.com/namanbarkiya",
+    link: "https://github.com/belpythons",
   },
   {
     name: "LinkedIn",
-    username: "Naman Barkiya",
+    username: "Belva Pranama",
     icon: Icons.linkedin,
-    link: "https://www.linkedin.com/in/namanbarkiya",
+    link: "https://www.linkedin.com/in/belva-pranama-01048a346/",
   },
   {
     name: "Twitter",
-    username: "@namanbarkiya",
+    username: "@bekgos8955",
     icon: Icons.twitter,
-    link: "https://twitter.com/namanbarkiya",
+    link: "https://x.com/bekgos8955?t=UVTHOJDdUwvAvso2N_Rrsg&s=08",
   },
   {
     name: "Gmail",
-    username: "naman.barkiya02",
+    username: "bebleblablibub",
     icon: Icons.gmail,
-    link: "mailto:naman.barkiya02@gmail.com",
+    link: "mailto:bebleblablibub@gmail.com",
   },
 ];
